28e1eb9991b89224b53e8c4aef0a779fe2c730cd
baumanl <laurabauman12@gmail.com>
2021-10-17 10:24:27 -0700

    added in split bill functionality

--- app/src/main/java/edu/stanford/baumanl/tippy/MainActivity.kt
+++ app/src/main/java/edu/stanford/baumanl/tippy/MainActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.SeekBar
+import android.widget.Switch
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import org.w3c.dom.Text
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
@@ -20,11 +23,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
     private lateinit var tvTipDescription: TextView
-
+    private lateinit var sSplit: Switch
+    private lateinit var etPartySize: EditText
+    private lateinit var tvPartySize: TextView
+    private lateinit var tvPerPersonTotal: TextView
+    private lateinit var tvPerPerson: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sSplit = findViewById(R.id.sSplit)
+        etPartySize = findViewById(R.id.etPartySize)
+        tvPartySize = findViewById(R.id.tvPartySize)
+        tvPerPerson = findViewById(R.id.tvPerPerson)
+        tvPerPersonTotal = findViewById(R.id.tvPerPersonTotal)
         etBaseAmount = findViewById(R.id.etBaseAmount)
         seekBarTip = findViewById(R.id.seekBarTip)
         tvTipPercentageLabel = findViewById(R.id.tvTipPercentageLabel)
@@ -56,6 +68,45 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
                 Log.i(TAG, "afterTextChanged $s")
                 computeTipAndTotal()
+            }
+
+        })
+
+        sSplit.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                //checked
+                Log.i(TAG, "checked")
+                etPartySize.visibility = View.VISIBLE
+                tvPartySize.visibility = View.VISIBLE
+                tvPerPersonTotal.visibility = View.VISIBLE
+                tvPerPerson.visibility = View.VISIBLE
+                etPartySize.setText("1")
+                tvPerPersonTotal.text = tvTotalAmount.text
+            } else {
+                //not checked
+                Log.i(TAG, "not checked")
+                etPartySize.visibility = View.INVISIBLE
+                tvPartySize.visibility = View.INVISIBLE
+                tvPerPersonTotal.visibility = View.INVISIBLE
+                tvPerPerson.visibility = View.INVISIBLE
+
+            }
+        }
+
+        etPartySize.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (tvTotalAmount.text.isEmpty() || etPartySize.text.isEmpty()) {
+                    tvPerPersonTotal.text = ""
+                    return
+                }
+                var total = tvTotalAmount.text.toString().toDouble()
+                var partySize = etPartySize.text.toString().toInt()
+                var res = total / partySize
+                tvPerPersonTotal.text = "%.2f".format(res)
             }
 
         })
